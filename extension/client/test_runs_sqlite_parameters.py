1779cb62ed630f4f011115dcd109396cb89851c9
ila <ilaria.battiston@gmail.com>
2025-05-27 11:41:45 +0200

    new params

--- extension/client/test_runs_sqlite_parameters.py
+++ extension/client/test_runs_sqlite_parameters.py
@@ -29,7 +29,7 @@ CHUNK_SIZE = 100 # Number of clients to process in each chunk
 INITIAL_CLIENTS = 2000 # Initial number of clients to simulate
 CLIENT_DISPATCH_INTERVAL = 5 # Interval in seconds to wait before dispatching new clients
 SLEEP_INTERVAL = 15 # Additional interval in seconds to sleep between dispatches
-FLUSH_INTERVAL = 5 # Interval in minutes to flush the database
+FLUSH_INTERVAL = 16 # Interval in minutes to flush the database
 MAX_RUNS = 5 # Maximum number of runs to simulate
 
 # Refresh parameters (note: it takes around 3 min to initialize 2000 clients)
